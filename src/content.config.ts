23febf34779b0370ebb61cada401469de9545cc9
piar <thomascreeves200@gmail.com>
2026-08-02 21:03:09 -0400

    :3

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,20 @@
+// idk much about typescript, this was in fact an absolute pain to figure out
+// comments are for me later
+
 import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
+import { glob } from "astro/loaders"; // deprecated? i think not.
+// allows you to load multiple local files at once essentially
 
 const posts = defineCollection({
   loader: glob({
-    pattern: "**/[^_]*.{md,mdx}",
-    base: "./src/content/posts",
+    pattern: "**/[^_]*.{md,mdx}", // goodness this is a mouthfull
+    // for future me, this basically just recursively matches subdirectories and finds md (markdown) files
+    base: "./src/content/posts", // self explanatory, this is the "base" 
   }),
-  schema: z.object({
+  schema: z.object({ // this uses zod by the way
     title: z.string(),
     //date: z.string(),
-    date: z.coerce.date(),
+    date: z.coerce.date(), // tries to convert to date if not already
     description: z.string().optional(),
     tags: z.array(z.string()).optional(),
   }),
