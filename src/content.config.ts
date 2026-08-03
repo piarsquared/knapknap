c598b4ee17cfb13147885c937f63e4e83774052b
piar <thomascreeves200@gmail.com>
2026-08-02 20:51:37 -0400

    fixed date

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,14 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const posts = defineCollection({
-
-  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/posts' }),
+  loader: glob({
+    pattern: "**/[^_]*.{md,mdx}",
+    base: "./src/content/posts",
+  }),
   schema: z.object({
     title: z.string(),
-    // date: z.string(),
+    //date: z.string(),
     date: z.coerce.date(),
     description: z.string().optional(),
     tags: z.array(z.string()).optional(),
