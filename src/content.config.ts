95787c8214f37fb50201bf7b5c388c918807afb1
piar <thomascreeves200@gmail.com>
2026-08-02 21:09:14 -0400

    SMILE! :D

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,8 @@
 // idk much about typescript, this was in fact an absolute pain to figure out
 // comments are for me later
 
+// name is content.config.ts, similar to __init__.py but for typescript obviously
+
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders"; // deprecated? i think not.
 // allows you to load multiple local files at once essentially
@@ -11,7 +13,7 @@ const posts = defineCollection({
     // for future me, this basically just recursively matches subdirectories and finds md (markdown) files
     base: "./src/content/posts", // self explanatory, this is the "base" 
   }),
-  schema: z.object({ // this uses zod by the way
+  schema: z.object({ // this uses zod by the way, a validation library
     title: z.string(),
     //date: z.string(),
     date: z.coerce.date(), // tries to convert to date if not already
